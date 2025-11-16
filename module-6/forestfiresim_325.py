84a2fac69204e766fa74903dd0f03189ed1caf0b
Isaac St Hubert <isthubert@my365.bellevue.edu>
2025-11-16 18:37:48 -0500

    Pushing 11/16/2025

---
+++ module-6/forestfiresim_325.py
@@ -0,0 +1,147 @@
+# Isaac St Hubert and Xavier Grunitzky Module 6.2
+# This program revises the original forestfiresim.py to include a lake area
+
+import random, sys, time
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+
+WIDTH = 79
+HEIGHT = 22
+
+TREE = 'A'
+FIRE = '@'
+EMPTY = ' '
+
+# Water symbol for the lake
+
+WATER = 'W'
+
+INITIAL_TREE_DENSITY = 0.20
+GROW_CHANCE = 0.01
+FIRE_CHANCE = 0.01
+
+
+PAUSE_LENGTH = 0.5
+
+
+def main():
+    forest = createNewForest()
+    bext.clear()
+
+    while True:
+        displayForest(forest)
+
+        nextForest = {'width': forest['width'], 'height': forest['height']}
+
+        for x in range(forest['width']):
+            for y in range(forest['height']):
+
+                # Water never changes
+
+                if forest[(x, y)] == WATER:
+                    nextForest[(x, y)] = WATER
+                    continue
+
+                if (x, y) in nextForest:
+
+                    continue
+
+                if ((forest[(x, y)] == EMPTY)
+                    and (random.random() <= GROW_CHANCE)):
+
+                    nextForest[(x, y)] = TREE
+
+                elif ((forest[(x, y)] == TREE)
+                    and (random.random() <= FIRE_CHANCE)):
+
+                    nextForest[(x, y)] = FIRE
+
+                elif forest[(x, y)] == FIRE:
+
+                    for ix in range(-1, 2):
+                        for iy in range(-1, 2):
+
+                            if forest.get((x + ix, y + iy)) == TREE:
+                                nextForest[(x + ix, y + iy)] = FIRE
+
+                    nextForest[(x, y)] = EMPTY
+
+                else:
+
+                    nextForest[(x, y)] = forest[(x, y)]
+        forest = nextForest
+
+        time.sleep(PAUSE_LENGTH)
+
+
+def createNewForest():
+
+    forest = {'width': WIDTH, 'height': HEIGHT}
+
+
+    # Variables that determine the center point
+
+    center_x = WIDTH // 2
+    center_y = HEIGHT // 2
+
+
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+
+            # Places the water symbol in specified coordinates to create a lake
+
+            if center_x - 5 < x < center_x + 5 and center_y - 5 < y < center_y + 5:
+                forest[(x, y)] = WATER
+                continue
+
+            if (random.random() * 100) <= INITIAL_TREE_DENSITY:
+                forest[(x, y)] = TREE
+            else:
+                forest[(x, y)] = EMPTY
+
+    return forest
+
+
+def displayForest(forest):
+    bext.goto(0, 0)
+
+    for y in range(forest['height']):
+        for x in range(forest['width']):
+
+            if forest[(x, y)] == TREE:
+                bext.fg('green')
+                print(TREE, end='')
+
+            elif forest[(x, y)] == FIRE:
+                bext.fg('red')
+                print(FIRE, end='')
+              
+            elif forest[(x, y)] == EMPTY:
+                print(EMPTY, end='')
+
+	    # Makes the water symbol blue
+
+            elif forest[(x,y)] == WATER:
+                bext.fg('blue')
+                print(WATER, end= '')
+
+        print()
+
+    bext.fg('reset')
+    print('Grow chance: {}%  '.format(GROW_CHANCE * 100), end='')
+    print('Lightning chance: {}%  '.format(FIRE_CHANCE * 100), end='')
+    print('Press Ctrl-C to quit.')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
